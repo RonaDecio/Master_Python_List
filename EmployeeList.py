b47f12f8960efdb89f28d8a9ec863fd46a54d5c8
RonaDecio <ronamoldezdecio14@gmail.com>
2024-10-12 22:04:24 +0800

    docs: adding Employee list

---
+++ EmployeeList.py
@@ -0,0 +1,9 @@
+employee_list = [
+    {"ID": 1, "Name": "John Bravo", "Department": "Sales", "Age": 30, "Email": "john.bravo@company.com"},
+    {"ID": 2, "Name": "kate Smith", "Department": "Human Resources", "Age": 25, "Email": "kate.smith@company.com"},
+    {"ID": 3, "Name": "Mark Lasi", "Department": "IT", "Age": 40, "Email": "mark.lasi@company.com"},
+    {"ID": 4, "Name": "Lyza Wrong", "Department": "Marketing", "Age": 28, "Email": "lyza.wrong@company.com"},
+    {"ID": 5, "Name": "Paula McDonald", "Department": "Finance", "Age": 35, "Email": "paula.mcdonald@company.com"}
+]
+
+print(employee_list)
